3770758a69996acbe69301ba7119de1520c20054
M-Osama96 <mohamedosama3466@gmail.com>
2025-10-22 20:43:50 +0300

    about

--- js/main.js
+++ js/main.js
@@ -1,50 +1,54 @@
-const menu_btn = document.querySelector(".menu-btn");
-const body = document.body;
-const header = document.querySelector(".main-header");
-menu_btn.addEventListener("click", function () {
-  header.classList.toggle("active");
-  body.classList.toggle("overflow");
-});
+if (document.querySelector(".menu-btn") !== null) {
+  const menu_btn = document.querySelector(".menu-btn");
+  const body = document.body;
+  const header = document.querySelector(".main-header");
+  menu_btn.addEventListener("click", function () {
+    header.classList.toggle("active");
+    body.classList.toggle("overflow");
+  });
+}
 
 Fancybox.bind("[data-fancybox]", {
   //
 });
 
-const tab = document.querySelector(".tabs");
-const tabButtons = tab.querySelectorAll('[role="tab"]');
-const tabPanels = Array.from(tab.querySelectorAll('[role="tabpanel"]'));
+if (document.querySelector(".tabs") !== null) {
+  const tab = document.querySelector(".tabs");
+  const tabButtons = tab.querySelectorAll('[role="tab"]');
+  const tabPanels = Array.from(tab.querySelectorAll('[role="tabpanel"]'));
 
-function tabClickHandler(e) {
-  console.log(123);
-  //Hide All Tabpane
-  tabPanels.forEach((panel) => {
-    // panel.hidden = "true";
-    panel.style.display = "none";
-  });
+  function tabClickHandler(e) {
+    console.log(123);
+    //Hide All Tabpane
+    tabPanels.forEach((panel) => {
+      // panel.hidden = "true";
+      panel.style.display = "none";
+    });
 
-  //Deselect Tab Button
+    //Deselect Tab Button
+    tabButtons.forEach((button) => {
+      button.setAttribute("aria-selected", "false");
+    });
+
+    //Mark New Tab
+    e.currentTarget.setAttribute("aria-selected", "true");
+
+    //Show New Tab
+    const { id } = e.currentTarget;
+
+    const currentTab = tabPanels.find(
+      (panel) => panel.getAttribute("aria-labelledby") === id
+    );
+
+    // currentTab.hidden = false;
+    currentTab.style.display = null;
+  }
+
   tabButtons.forEach((button) => {
-    button.setAttribute("aria-selected", "false");
+    button.addEventListener("click", tabClickHandler);
+    console.log(button);
   });
-
-  //Mark New Tab
-  e.currentTarget.setAttribute("aria-selected", "true");
-
-  //Show New Tab
-  const { id } = e.currentTarget;
-
-  const currentTab = tabPanels.find(
-    (panel) => panel.getAttribute("aria-labelledby") === id
-  );
-
-  // currentTab.hidden = false;
-  currentTab.style.display = null;
 }
-
-tabButtons.forEach((button) => {
-  button.addEventListener("click", tabClickHandler);
-  console.log(button);
-});
 
 var swiper = new Swiper(".mySwiper", {
   navigation: {
@@ -57,38 +61,40 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-let elementsArray = document.querySelectorAll(".clients-cont");
-window.addEventListener("scroll", fadeIn);
-function fadeIn() {
-  for (var i = 0; i < elementsArray.length; i++) {
-    var elem = elementsArray[i];
-    var distInView = elem.getBoundingClientRect().top - window.innerHeight + 0;
-    console.log(distInView);
-    if (distInView < 0) {
-      elem.classList.add("inView");
-    } else {
-      elem.classList.remove("inView");
+if (document.querySelectorAll(".clients-cont") !== null) {
+  let elementsArray = document.querySelectorAll(".clients-cont");
+  window.addEventListener("scroll", fadeIn);
+  function fadeIn() {
+    for (var i = 0; i < elementsArray.length; i++) {
+      var elem = elementsArray[i];
+      var distInView =
+        elem.getBoundingClientRect().top - window.innerHeight + 0;
+      console.log(distInView);
+      if (distInView < 0) {
+        elem.classList.add("inView");
+      } else {
+        elem.classList.remove("inView");
+      }
     }
   }
+  fadeIn();
 }
-fadeIn();
 
-const accordionHeaders = document.querySelectorAll(".accordion-header");
+if (document.querySelectorAll(".accordion-header") !== null) {
+  const accordionHeaders = document.querySelectorAll(".accordion-header");
 
-accordionHeaders.forEach((header) => {
-  header.addEventListener("click", () => {
-    const item = header.parentElement;
-    const openItem = document.querySelector(".accordion-item.active-a");
+  accordionHeaders.forEach((header) => {
+    header.addEventListener("click", () => {
+      const item = header.parentElement;
+      const openItem = document.querySelector(".accordion-item.active-a");
 
-    // لو فيه عنصر مفتوح غير الحالي → يقفله
-    if (openItem && openItem !== item) {
-      openItem.classList.remove("active-a");
-    }
+      // لو فيه عنصر مفتوح غير الحالي → يقفله
+      if (openItem && openItem !== item) {
+        openItem.classList.remove("active-a");
+      }
 
-    // يقلب حالة العنصر الحالي (فتح / غلق)
-    item.classList.toggle("active-a");
+      // يقلب حالة العنصر الحالي (فتح / غلق)
+      item.classList.toggle("active-a");
+    });
   });
-});
-
-// فتح أول عنصر افتراضياً (لو مش محطوط في HTML)
-document.querySelector(".accordion-item:first-child").classList.add("active-a");
+}
